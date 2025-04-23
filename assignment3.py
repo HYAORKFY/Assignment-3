d5809066a08baf1fbcb92ce6bf62db9622509a76
HYAORKFY <152956209+HYAORKFY@users.noreply.github.com>
2025-04-23 09:48:55 +0800

    Initial project structure
    Initial my project structure with basic imports and function stubs

---
+++ assignment3.py
@@ -0,0 +1,16 @@
+import socket
+
+def send_request_to_server(host, port, filename):
+    """Send each line of the file as a request to the server."""
+    pass
+
+def main():
+    """Main function to start the client and server."""
+    pass
+
+def start_server():
+    """The TCP server that will handle client connections."""
+    pass
+
+if __name__ == '__main__':
+    main()
