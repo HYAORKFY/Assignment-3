b48d8d012da71b0b2c82186c14237c34d106b6cb
HYAORKFY <152956209+HYAORKFY@users.noreply.github.com>
2025-04-23 10:26:51 +0800

    Create a simple send_request_to_server function
    Create a simple send_request_to_server function to complete the creation and connection of the client socket

--- assignment3.py
+++ assignment3.py
@@ -2,14 +2,18 @@ import socket
 
 def send_request_to_server(host, port, filename):
     """Send each line of the file as a request to the server."""
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    client_socket.close()
+
+
+def start_server():
+    """The TCP server that will handle client connections."""
     pass
 
 def main():
     """Main function to start the client and server."""
-    pass
-
-def start_server():
-    """The TCP server that will handle client connections."""
     pass
 
 if __name__ == '__main__':
